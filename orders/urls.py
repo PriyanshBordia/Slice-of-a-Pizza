bc8baf447b9692a8455a06db6627ba50f8f25402
PriyanshBordia <priyanshbordia1@gmail.com>
2020-07-03 19:18:32 +0530

    added html content

--- orders/urls.py
+++ orders/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("register", views.register_view, name="register"),
     path("login", views.login_view, name="login"),
-    # path("", views., name=""),
+    # path("cart", views.cart_view, name="cart"),
     # path("", views., name=""),
     # path("", views., name=""),
 ]
--- orders/views.py
+++ orders/views.py
@@ -3,9 +3,9 @@ import time
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse, HttpResponseRedirect, Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
-
+from django.contrib import admin
 
 # Create your views here.
 def index(request):
@@ -16,10 +16,31 @@ def index(request):
         return render(request, "orders/index.html", context={"pinnoconame": request.user})
 
 
+def register_view(request):
+
+    first = request.POST["first"]
+    last = request.POST["last"]
+    username = request.POST["username"]
+    email = request.POST["email"]
+    password = request.POST["password"]
+
+    # Check for username if present in records if yes, raise error
+        # 1. Already a user
+        # 2. username unavailable
+    if username in User:
+        return render(request, "orders/error.html", context={"message": "Username Already taken!"})
+
+    if password != re_password:
+        return render(request, "orders/error.html", context={"message": "Passwords don't match"})
+
+    user = User(first_name=first, last_name=last, username=username, email=email, password=password, is_staff=False)
+    user.save()
+
+
 def login_view(request):
 
-    username = request.POST["username"]
-    password = request.POST["password"]
+    username = request.POST.get("username", False)
+    password = request.POST.get("password", False)
 
     user = authenticate(request, username=username, password=password)
     if user is not  None:
@@ -32,26 +53,11 @@ def login_view(request):
         user = User.objects.get()
         return render(request, "orders/menue.html", context={"user": user})
 
+
 def logout_view(request):
     logout(request)
     return render(request, "orders/login.html")
 
-def register_view(request):
 
-    first = request.POST["first"]
-    last = request.POST["last"]
-    username = request.POST["username"]
-    email = request.POST["email"]
-    password = request.POST["password"]
-
-# Check for username if present in records if yes, raise error
-        # 1. Already a user
-        # 2. username unavailable
-    if username in User:
-        return render(request, "orders/error.html", context={"message": "Username Already taken!"})
-
-    if password != re_password:
-        return render(request, "orders/error.html", context={"message": "Passwords don't match"})
-
-    user = User(first_name=first, last_name=last, username=username, email=email, password=password, is_staff=False)
-    user.save()
+def add_item(request):
+    pass
