2bb15276658079da90ee60e524e289c2f16884d4
PriyanshBordia <priyanshbordia1@gmail.com>
2021-06-19 14:18:53 +0530

    should work

--- pizza/settings.py
+++ pizza/settings.py
@@ -10,13 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
-import os
-from pathlib import Path
 import environ
 
 env = environ.Env()
 environ.Env.read_env()
 
+import os
+from pathlib import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -80,6 +80,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'pizza.wsgi.application'
 
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
@@ -89,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
-	
+
    	'development': {
         'ENGINE': env('DEV_ENGINE'),
         'NAME': env('DEV_DB_NAME'),
