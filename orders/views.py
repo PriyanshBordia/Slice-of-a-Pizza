cab25e54380e6628a5f65383a1d09309638b43c4
Restyled.io <commits@restyled.io>
2021-11-12 12:57:34 +0000

    Restyled by autopep8

--- orders/views.py
+++ orders/views.py
@@ -8,6 +8,8 @@ from django.urls import reverse
 from django.contrib import admin
 
 # Create your views here.
+
+
 def index(request):
     if not request.user.is_authenticated:
         return render(request, "orders/login.html")
@@ -24,15 +26,16 @@ def register_view(request):
     password = request.POST["password"]
 
     # Check for username if present in records if yes, raise error
-        # 1. Already a user
-        # 2. username unavailable
+    # 1. Already a user
+    # 2. username unavailable
     if username in User:
         return render(request, "orders/error.html", context={"message": "Username Already taken!"})
 
     if password != re_password:
         return render(request, "orders/error.html", context={"message": "Passwords don't match"})
 
-    user = User(first_name=first, last_name=last, username=username, email=email, password=password, is_staff=False)
+    user = User(first_name=first, last_name=last, username=username,
+                email=email, password=password, is_staff=False)
     user.save()
 
 
@@ -42,7 +45,7 @@ def login_view(request):
     password = request.POST.get("password", False)
 
     user = authenticate(request, username=username, password=password)
-    if user is not  None:
+    if user is not None:
         login(request, user)
 
     if User.objects.get(username=username, password=password).count() == 0:
